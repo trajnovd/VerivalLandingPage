146cc9809eb06cea610550ae16abbdf5f51bfcdb
krstevski02 <krstevski02@gmail.com>
2026-06-19 15:31:02 +0200

    Ermina's photo

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -33,7 +33,7 @@ const team = [
       si: 'Pooblaščena ocenjevalka vrednosti nepremičnin in strokovna nosilka domenskega področja v projektu reValu8. Magistrica ekonomije (finance) in univ. dipl. inž. elektrotehnike z več kot 15-letnimi izkušnjami v insolvenčnih postopkih, kjer so cenitve ključno orodje za odločanje.',
     },
     initials: 'EB',
-    image: null,
+    image: '/images/Ermina Bender pp.jpg',
     gradient: 'from-emerald-500 to-green-500',
   },
   {
